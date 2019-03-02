d0f37f48ba83a91a925ab16325bb5253fc451a8b
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-02 16:35:55 +0000

    H-numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,4 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 3 * 5;
+var numberOfMentors = 16 / 2;
+console.log(numberOfStudents + numberOfMentors);
