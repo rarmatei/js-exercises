7b9294d5970d1484c10ed3cd7e62a9bf72712d10
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-02 15:35:42 +0000

    F-strings-concatenation

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var greetingfirst = "Hey nice to meet you";
+var name = "Mohamed";
+var greeting = greetingfirst + name;
+console.log(greeting);
