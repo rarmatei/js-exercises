9b2c3d35b381d2055c8681dec34b45a856951476
Adnan <adnanmh82@gmail.com>
2019-03-03 23:34:33 +0000

    exercise js numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var total = 15 + 8;
+var sum = "Total number of students and mentors :";
+console.log("Number of students :" + numberOfStudents);
+console.log("Number of mentors :" + numberOfmentors);
+console.log(sum + total);
