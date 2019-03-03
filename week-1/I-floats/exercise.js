ab63213f8a38738be5eb336e65635e0b055990c3
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-03 13:15:47 +0000

    I-floats

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,12 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var totalNumber = numberOfStudents + numberOfMentors;
+
+var studentPercent = (numberOfStudents / totalNumber) * 100;
+var roundPercent = Math.round(studentPercent);
+console.log("Percentage students: " + roundPercent + "%");
+
+var mentorsPercent = (numberOfMentors / totalNumber) * 100;
+
+console.log("Percentage mentors: " + Math.round(mentorsPercent) + "%");
