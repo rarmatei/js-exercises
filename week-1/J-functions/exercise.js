c73852fa2f7b50a63fecd3dbfc6045d726b1b972
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-03 14:48:24 +0000

    J-functions

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,10 @@
 function halve(number) {
-  // complete the function here
+  return number / 2;
 }
 
-var result = halve(12);
-
-console.log(result);
+var result1 = halve(12);
+var result2 = halve(16);
+var result3 = halve(18);
+console.log(result1);
+console.log(result2);
+console.log(result3);
