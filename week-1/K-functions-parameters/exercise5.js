28afe4b3af1d96814fa1a6449acf93d7792a842e
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-03 20:24:49 +0000

    K-function-parameters exercise 5

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,6 +1,7 @@
 // Declare your function here
 function createLongGreeting(name, age) {
-  return "Hello, my name is" + name + "and I'm " + age + " years old";
+  var message = "Hello, my name is" + name + "and I'm " + age + " years old";
+  return message;
 }
 const greeting = createLongGreeting("Daniel", 30);
 
