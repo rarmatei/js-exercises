0a01894dcf8d8ac3e056c15ff37ef6df6ff3d782
coder <coder@cyf10-0-2-26.codeyourfuture.io>
2019-03-04 20:20:05 +0000

    WEEK-1 exercises

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,10 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = 15 + 8;
+
+var percentageStudents = Math.round((100 * 15) / total);
+var percentageMentors = Math.round((100 * 8) / total);
+
+ 
+ console.log("Percentage students: " + percentageStudents + "%");
+ console.log("Percentage mentors: " + percentageMentors + "%");
