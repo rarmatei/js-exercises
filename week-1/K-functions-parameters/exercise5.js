8d615c825409e06c2a4aeff51a1baf12b0e7854c
Meriem <mmlys89@gmail.com>
2019-03-06 18:23:06 +0000

    modifiying number 5, by declaring two variable name and age

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -2,9 +2,11 @@
 function createLongGreeting(name, age) {
   return name, age;
 }
+var name = "Daniel";
+var age = 30;
 const greeting = createLongGreeting("Daniel", 30);
 
-console.log("Hello, my name is Daniel and I'm", greeting, "years old");
+console.log("Hello, my name is", name, "and I'm", age, "years old");
 
 // ignore below
 
