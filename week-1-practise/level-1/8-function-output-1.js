74dd3a7e9986c9701edd67ee7127962b5a10493d
coder <coder@cyf10-0-2-26.codeyourfuture.io>
2019-03-06 00:16:09 +0000

    week-1-practises

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
+// - What does this program do? -it takes random number between  1.0....9.0
 // - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What is the function name?  -getNumber
+// - Where is the function called? with what parameters?  -result
--- week-1-practise/level-2/3-currency-conversion.js
+++ week-1-practise/level-2/3-currency-conversion.js
@@ -5,7 +5,11 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+
+  return "$" + (price * 1.4).toFixed(2);
+  
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
