7d2edd2f4db4ac20721251d4499f5f612a61ba9e
Meriem <mmlys89@gmail.com>
2019-03-06 16:23:35 +0000

    using sum this time to add two numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,7 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
 var numberOfStudents = "Number of students: 15";
 var numberOfMentors = "Number of mentors: 8";
-var total = "Total number of students and mentors: 23";
+var total = "Total number of students and mentors:";
+var sum = 15 + 8;
 console.log(numberOfStudents);
 console.log(numberOfMentors);
-console.log(total);
+console.log(total, sum);
