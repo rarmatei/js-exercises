26a734ac9a896b4d280f7f14eeb60d2af70d20cc
Meriem <mmlys89@gmail.com>
2019-03-06 16:18:38 +0000

    Finishing this exercice but not sure if it's the right methode as I didn't use any addition

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = "Number of students: 15";
+var numberOfMentors = "Number of mentors: 8";
+var total = "Total number of students and mentors: 23";
+console.log(numberOfStudents);
+console.log(numberOfMentors);
+console.log(total);
