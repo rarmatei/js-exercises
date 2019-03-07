4dba33513019d09503ec1cab8426067704a7a1be
Adnan <adnanmh82@gmail.com>
2019-03-07 16:06:14 +0000

    H numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -4,5 +4,5 @@ var numberOfMentors = 8;
 var total = 15 + 8;
 var sum = "Total number of students and mentors :";
 console.log("Number of students :" + numberOfStudents);
-console.log("Number of mentors :" + numberOfmentors);
+console.log("Number of mentors :" + numberOfMentors);
 console.log(sum + total);
