962de94f15dcfca0c5e6c31527a55dfaa63af40b
youssef910 <youssef910>
2019-03-07 21:33:01 +0000

    completed level 1 practice

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? this program chose by the (Math.random() method a number which is between 0,1 randomly and at can be any number betwen 0 and 1 but not 1.)multiblied by 10 so the result as any number between 0 and 1 *10
+// - How many parameters does the function take?0 or no difinet parameters
+// - What is the function name?getNumber
+// - Where is the function called?in the var and named result   with what parameters?
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -2,13 +2,13 @@ function s(w1, w2) {
   return w1.concat(w2);
 }
 
-var result = s("code", "your");
+var result = s("code ", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. the function joins the 2 parameter strings  into one line
+// - How many parameters does the function take? 2 (w1 ,w2)
+// - What is the function name?  s
+// - Where is the function called? var result  with what parameters? ("code","your")
+// - What does the function return? codeyour
+// - How can this function be improved? adding spaces inside first string parameter  ("code ") the return will be (code your)
