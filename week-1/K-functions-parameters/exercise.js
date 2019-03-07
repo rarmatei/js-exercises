e452f5676ce66b43dc79558a5da2839438700d76
Adnan <adnanmh82@gmail.com>
2019-03-07 16:13:25 +0000

    K function parameter

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,6 +1,7 @@
 // Complete the function so that it takes input parameters
-function multiply() {
+function multiply(three, four) {
   // Calculate the result of the function and return it
+  return three * four;
 }
 
 // Assign the result of calling the function the variable `result`
--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,6 +1,10 @@
 // Declare your function first - call it "add"
+function add(one, two) {
+  return one + two;
+}
 
 // Call the function and assign to a variable `sum`
+var sum = add(124, 13);
 
 console.log(sum);
 
