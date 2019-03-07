e516b89ebacbdb69960af9269b8ffe21b3d3816c
youssef910 <youssef910>
2019-03-07 03:25:31 +0000

    completed ex K functuions parameters

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,7 +1,9 @@
 // Declare your function first - call it "add"
-
+function add(a, b) {
+  return a + b;
+}
 // Call the function and assign to a variable `sum`
-
+var sum = add(13, 124);
 console.log(sum);
 
 // ignore below
