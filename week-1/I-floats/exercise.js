bba3402188136c64c29ed55f37396fff0bfcf050
Adnan <adnanmh82@gmail.com>
2019-03-07 16:07:48 +0000

    I floats

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percentageOfStudents = (15 * 100) / 23;
+var roughstudents = Math.round(percentageOfStudents);
+var percentageofmentors = (8 * 100) / 23;
+var roughmentors = Math.round(percentageofmentors);
+console.log("Percentage students: " + roughstudents + "%");
+console.log("Percentage mentors: " + roughmentors + "%");
