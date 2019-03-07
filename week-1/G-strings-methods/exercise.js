956bcee0e2cc0c34f7c2e88280e765f42c66efe2
youssef910 <youssef910>
2019-03-07 01:38:47 +0000

    con=mpleted ex G part 1

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,5 +1,7 @@
 // Start by creating a variable `name`
 var name = "Youssef";
 var nameLength = name.length;
-// console.log("My name is " + name + " and name is " + nameLength+ " characters long");
 console.log(nameLength);
+console.log(
+  "My name is " + name + " and my name is " + nameLength + " characters long"
+);
