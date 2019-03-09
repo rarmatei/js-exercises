f6326e04b4db1358780fc986958e3dbe433a0396
Phil Ezemonye <philezemonye@gmail.com>
2019-03-09 12:29:08 +0000

    C-comparison-operators/exercise.js

--- week-2/C-comparison-operators/exercise.js
+++ week-2/C-comparison-operators/exercise.js
@@ -7,14 +7,14 @@
 
 var studentCount = 16;
 var mentorCount = 9;
-var moreStudentsThanMentors; // finish this statement
+var moreStudentsThanMentors = 16 > 9;
 
 var roomMaxCapacity = 25;
-var enoughSpaceInRoom; // finish this statement
+var enoughSpaceInRoom = 25 > 16;
 
 var personA = "Daniel";
 var personB = "Irina";
-var sameName; // finish this statement
+var sameName = personA === personB;
 
 /* 
   PLEASE IGNORE LINES BELOW
