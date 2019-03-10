bded88cd9463068922043c6dde24150c4ebe4901
Nabraha <nebaytedros@yahoo.com>
2019-03-10 11:53:38 +0000

    complete-week-2-level-2

--- week-2-practise/level-2/2-validation.js
+++ week-2-practise/level-2/2-validation.js
@@ -1,11 +1,30 @@
+import { numberLiteralTypeAnnotation, returnStatement } from "@babel/types";
+
 // Complete the function to check if the variable `num` satisfies the following requirements:
 // - is a number
 // - is a positive number
 // - is less than or equal to 100
 // Tip: write other small functions for each requirement
 
-function validate(num) {}
+function validate(num) {
+  return isNumber(num) && isPositive(num) && isLessThanOrEqual(num);
+}
 
+function isNumber(number) {
+  return typeof number === "number";
+}
+
+function isPositive(number) {
+  // var psotiveNumer = number > 0;
+  // return psotiveNumer ;
+  return number >= 0;
+}
+
+function isLessThanOrEqual(number) {
+  // var lessThanOrEqual = number <= 100;
+  // return lessThanOrEqual;
+  return number <= 100;
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
