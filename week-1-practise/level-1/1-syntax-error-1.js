9cce1d86d4da64d67e6ad6c49327b2d1ae0c590f
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-12 16:51:36 +0000

    changed files-level1-1-4-5-6

--- week-1-practise/level-1/1-syntax-error-1.js
+++ week-1-practise/level-1/1-syntax-error-1.js
@@ -8,6 +8,6 @@ var result = addNumbers(1, 3, 4);
 console.log(result); // Expected result 8 - Run the program and make sure it works
 
 // Answer these questions:
-// 1. How many parameters does the function take?
-// 2. What is the function name?
-// 3. Where is the function called? with what parameters?
+// 1. How many parameters does the function take? Three
+// 2. What is the function name? addNumber
+// 3. Where is the function called? with what parameters? var result
