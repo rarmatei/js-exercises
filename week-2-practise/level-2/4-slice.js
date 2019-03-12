da16f170b87fe09317a6a5dc82535afa6425583e
Blaise <blaiselubuma@gmail.com>
2019-03-12 20:03:19 +0000

    completed 4-slice.js

--- week-2-practise/level-2/4-slice.js
+++ week-2-practise/level-2/4-slice.js
@@ -4,8 +4,8 @@
 // - return a new array with the item removed
 // - remove the item at the specified index
 
-function remove(arr, index) {
-  return; // complete this statement
+function remove(numbers) {
+  return numbers.slice(0, 2); // complete this statement
 }
 
 /* 
