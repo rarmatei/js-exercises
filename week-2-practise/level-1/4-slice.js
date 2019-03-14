0fceb28d86ede5b375795355a675e5d4470728a0
Phil Ezemonye <philezemonye@gmail.com>
2019-03-14 17:03:32 +0000

    Completed level-1/4-slice.js

--- week-2-practise/level-1/4-slice.js
+++ week-2-practise/level-1/4-slice.js
@@ -1,6 +1,9 @@
 // Write a function `first5` that:
 // - returns the first 5 items from a provided array
-
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+function first5(num) {
+  return num.slice(0, 5);
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
