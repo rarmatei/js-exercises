1d4bd50f0a16b1db9dce5668d5e5e18e596bf722
Adnan <adnanmh82@gmail.com>
2019-03-18 00:26:35 +0000

    F-array for each

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -9,7 +9,22 @@
 */
 
 // Complete this function -->
-function fizzBuzz() {}
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+function fizzBuzz(number) {
+  if (number % 3 === 0) {
+    return "Fizz";
+  }
+  if (number % 5 === 0) {
+    return "Buzz";
+  }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  }
+  return number;
+}
+numbers.map(fizzBuzz).forEach(function(number) {
+  console.log(number);
+});
 
 // DEBUG
 var sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
