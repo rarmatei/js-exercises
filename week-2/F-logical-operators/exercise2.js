3242cfd0ffdf7236e25c50fb4ba0ce8306d84d03
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-03-20 22:28:25 +0000

    F-logical-operator

--- week-2/F-logical-operators/exercise2.js
+++ week-2/F-logical-operators/exercise2.js
@@ -5,8 +5,19 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
-
+function isNegative(num) {
+  return num < 0 ;
+}
+function isBetween5and10(num) {
+  return num >= 5 && num <= 10;
+}
+function isLessThan8Characters(name) {
+  return name.length < 8;
+}
+function startsWithD(letter) {
+  letter = "D";
+  return true;
+}
 /*
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
