704c8dccc26c0d19fce40644ad0878de5427876f
coder <coder@cyf10-0-2-26.codeyourfuture.io>
2019-03-30 14:32:45 +0000

    B-objects-get-set

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  brand: "Lenova",
+  screenSize: 10,
+  isTouchScreen: false,
+  hasCamera: true,
+  batteryTime: "3 hours"
+};
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,30 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+
+var dog ={
+    name:'Golge',
+    canFly: false,
+    color: 'brown',
+    numberOfLegs: 4,
+};
+
+var flower = {
+    name: 'Rose',
+    color: 'Pink and yellow',
+    placeIn:'West of Scotland',    
+};
+
+var artist ={
+    name: 'Picassso',
+    ageHeDied: 78,
+    painting : ['Guernica', 'The Old Guitarist', 'Bulls Head'],
+};
+ 
+var galaxies ={
+    name: 'Pluto',
+    chemicalComposition:'Mixture of %70 Rock',
+    radius: 1150,
+};
+
+var 
