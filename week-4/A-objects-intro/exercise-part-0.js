a76515e435952ed11ad559611b473e9f575bb5c4
Adnan <adnanmh82@gmail.com>
2019-03-30 13:07:20 +0000

    completed A objects

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var myLaptop = {
+  screenSize: 20,
+  brand: "Toshiba",
+  memory: "3Gb",
+  cpu: "core i5"
+};
+console.log(myLaptop);
