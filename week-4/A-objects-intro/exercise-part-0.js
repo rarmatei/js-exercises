a45d76aed781057965b4114ea2644c02f9e0564a
nabraha <nebaytedros@yahoo.com>
2019-03-30 13:02:07 +0000

    week-4-A-

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  name: 'Asus',
+  RAM: '8GB',
+  HardDrive: 'SSD'
+}
+
+console.log(laptop.name);
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,22 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+
+var Basketball ={
+  pointGuard: 'DRose',
+  powerForard: 'D-Rodman',
+  shootingGuard: 'step',
+      players: {
+        shoot: function(){
+          console.log('3 point');
+        },
+        name: 'nab',
+        shoesSize: 12
+      }
+}
+
+//console.log(Basketball.players.name);
+
+Basketball.players.shoot();
+
+//console.log(Basketball.players.shoot);
