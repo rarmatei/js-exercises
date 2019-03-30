b73f2cec5f507398c8f2993f9adb5d62e6cb5c5e
Adnan <adnanmh82@gmail.com>
2019-03-30 15:06:27 +0000

    completed c objects

--- week-4/C-more-complex-objects/exercise-3.js
+++ week-4/C-more-complex-objects/exercise-3.js
@@ -31,13 +31,26 @@ var parkAvenueHouse = {
 */
 
 // returns the full name (first name + last name) of the owner of the house
-function getOwnerFullName(house) {}
+function getOwnerFullName(house) {
+  var firstName = house.currentOwner.firstName;
+  var lastName = house.currentOwner.lastName;
+  return firstName + " " + lastName;
+}
 
 // returns an array of the owners' email addresses of the two houses
-function getEmailAddresses(house1, house2) {}
+function getEmailAddresses(house1, house2) {
+  var email1 = house1.currentOwner.email;
+  var email2 = house2.currentOwner.email;
+  return email1 + ", " + email2;
+}
 
 // returns the address for the cheapest house out of the two
-function getCheapestAddress(house1, house2) {}
+function getCheapestAddress(house1, house2) {
+  if (house1.price < house2.price) {
+    return house1.address;
+  }
+  return house2.address;
+}
 
 /*
   DO NOT EDIT ANYTHING BELOW THIS LINE
