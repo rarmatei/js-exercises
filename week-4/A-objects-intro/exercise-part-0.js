bd035e47c74ac7ba638df482f45d1083a5372fdf
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-04-02 17:20:40 +0100

    A-objects-intro

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,32 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  screenSize: 10,
+  memory:15,
+  type:"sony",
+  oldLaptop:true,
+  owners:["Nate","Luke"],
+}
+
+var laptop2 = {
+  screenSize: 10,
+  memory: 15,
+  type: {
+    name:"apple",
+    year:2018,
+  },
+  oldLaptop: true,
+  owners: ["Nate", "Luke"],
+}
+
+var mac = {
+  name:"macbook",
+  year:2016,
+}
+var macbook ={
+  name:"macbookbro",
+  year:2018,
+}
+console.log(laptop.type);
+console.log(laptop2.type);
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,28 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var room ={
+  numberOfDoors:3,
+  numberOfWindows:9,
+  name:"kinning park",
+}
+var trainingCentre ={
+  numberOfDoors: 15,
+  numberOfWindows: 25,
+  name: "community centre",
+}
+var way ={
+  streetName: "paisly road",
+  area:"Glasgow",
+  streetNmuber:15,
+}
+var city= {
+  adress:Glasgow,
+  number:23,
+  street:"argyle"
+}
+var weather = {
+  moring = 20,
+  day = "saturday",
+  howMany:1,
+}
